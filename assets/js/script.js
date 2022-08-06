241bdbcb1ed0cbe572bde1ec6b42bf22428f53c6
Jatin_Saini <sainijatin247@gmail.com>
2022-08-06 15:55:03 -0400

    drag-drop added

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,7 +79,7 @@ $(".list-group").on("click", "p", function(){
   $(this).replaceWith(textInput);
   textInput.trigger("focus");
   
-})
+});
 $(".list-group").on("blur","textarea",function(){
   var text = $(this).val().trim();
   var status = $(this).closest(".list-group").attr("id").replace("list-", "");
@@ -91,9 +91,67 @@ $(".list-group").on("blur","textarea",function(){
 
   $(this).replaceWith(taskP);
 
-}) 
+});
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event, ui){
+    console.log(ui);
+  },
+  deactivate: function(event, ui){
+    console.log(ui);
+  },
+  over: function(event){
+    console.log(event);
+  },
+  out: function(event){
+    console.log(event);
+  },
+  update: function(){
+    var tempArr = [];
+    $(this).children().each(function(){
+      var text = $(this).find("p").text().trim();
 
+      var date = $(this).find("span").text().trim();
+
+      tempArr.push({
+        text: text,
+        date: date
+      });
+
+           
+    });
+    
+          // trim down list's ID to match object property
+          var arrName = $(this).attr("id").replace("list-", "");
+
+          // update array on tasks object and save
+          tasks[arrName] = tempArr;
+          saveTasks();
+    
+  },
+  stop:function(event){
+    $(this).removeClass("dropover");
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
 
 
 // modal was triggered
